78da0a88793af50d4875b68c0ce863a6b55a6a1b
Francesco Casadei <fc@francescos-macbook-pro.home>
2020-04-09 14:40:54 +0200

    static server

--- src/App.js
+++ src/App.js
@@ -13,65 +13,30 @@ import {
   Redirect
 } from "react-router-dom";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/foo/one">About One</Link></li>
-            <li><Link to="/bar/two">About Two</Link></li>
-            <li><Link to="/splat">Splat</Link></li>
-          </ul>
-          <hr />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/foo/one" component={One} />
-            <Route path="/bar/two" component={Two} />
-            <Route component={Splat} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+<Router>
+<Switch>
+<Route exact path="/" component={MainComponent} />
+<Route exact path="/CoachingComponent" component={CoachingComponent} />
+<Route exact path="/ChanteuseComponent" component={ChanteuseComponent} />
+</Switch>
+{/*
+<ScrollToRoute exact path="/" component={MainComponent} />
+<ScrollToTopRoute exact path="/CoachingComponent" component={CoachingComponent} />
+<ScrollToTopRoute exact path="/ChanteuseComponent" component={ChanteuseComponent} />
+<ScrollToTopRoute exact path="/ContactComponent" component={ContactComponent} />
+
+
+<NavBar/>
+<HeaderComponent logo={logo}/>
+<CardsComponent margotImgCard1={margotImgCard1} margotImgCard2={margotImgCard2}/>
+<YouTubeComponent/>
+<Footer/>
+*/}
+
+</Router>
+  );
 }
 
-//State less components
-//Home
-const Home = () =>
-  <div>
-    <h1>Home</h1>
-    <p>This is the Home Page</p>
-  </div>;
-
-const One = () =>
-  <div>
-    <About />
-    <h1>One</h1>
-    <p>This is One</p>
-  </div>;
-
-const Two = () =>
-  <div>
-    <About />
-    <h1>Two</h1>
-    <p>This is Two</p>
-  </div>;
-
-//About
-const About = () =>
-  <div>
-    <h1>About</h1>
-    <p>This is about</p>
-  </div>;
-
-const Splat = () =>
-  <div>
-    <h1>Splat</h1>
-    <p>This is splat</p>
-  </div>;
-  
-export default App
+export default App;
